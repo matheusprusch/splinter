0cf365deae07c5bcf225a7235b765578aa3ac021
Matheus Prusch <matheusprusch@hotmail.com>
2016-05-15 19:06:24 -0300

    Heroku config

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
